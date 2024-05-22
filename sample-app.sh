3fff68afe2f8330a8e78377a9dd0c525ffcd3281
fxrnan <165220784+fxrnan@users.noreply.github.com>
2024-05-21 21:45:59 -0400

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-mkdir tempdir
-mkdir tempdir/templates
-mkdir tempdir/static
-
-cp sample_app.py tempdir/.
-cp -r templates/* tempdir/templates/.
-cp -r static/* tempdir/static/.
-
 echo "FROM python" >> tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
